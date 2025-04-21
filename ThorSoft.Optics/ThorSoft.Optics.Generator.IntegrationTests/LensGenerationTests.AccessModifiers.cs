84fec1cc7156cc75a5228cc04ef82010cb4fbc7f
Thorsten Reichert <ThorstenReichert@users.noreply.github.com>
2025-04-21 14:08:43 +0200

    Add structured tests for access-modifiers, record-declarations and property-declarations (#11)

---
+++ ThorSoft.Optics/ThorSoft.Optics.Generator.IntegrationTests/LensGenerationTests.AccessModifiers.cs
@@ -0,0 +1,97 @@
+﻿namespace ThorSoft.Optics.Generator.IntegrationTests
+{
+    [GenerateLenses]
+    public sealed partial record class AccessModifierPublic
+    {
+        public int Property { get; init; }
+    }
+
+    [GenerateLenses]
+    internal sealed partial record class AccessModifierInternal
+    {
+        internal int Property { get; init; }
+    }
+
+    [GenerateLenses]
+    public partial record class AccessModifierProtected
+    {
+        protected int Property { get; init; }
+    }
+
+    public record class AccessModifierProtectedTest : AccessModifierProtected
+    {
+        public static void Test()
+        {
+            var instance = new AccessModifierProtectedTest { Property = 1 };
+
+            Assert.Equal(5, PropertyLens.Get(PropertyLens.Set(5, instance)));
+        }
+    }
+
+    [GenerateLenses]
+    public partial record class AccessModifierPrivateProtected
+    {
+        private protected int Property { get; init; }
+    }
+
+    public record class AccessModifierPrivateProtectedTest : AccessModifierPrivateProtected
+    {
+        public static void Test()
+        {
+            var instance = new AccessModifierPrivateProtectedTest { Property = 1 };
+
+            Assert.Equal(5, PropertyLens.Get(PropertyLens.Set(5, instance)));
+        }
+    }
+
+    [GenerateLenses]
+    public partial record class AccessModifierPrivate
+    {
+        private int Property { get; init; }
+
+        public static void Test()
+        {
+            var instance = new AccessModifierPrivate { Property = 1 };
+
+            Assert.Equal(5, PropertyLens.Get(PropertyLens.Set(5, instance)));
+        }
+    }
+
+    public sealed partial class LensGenerationTests
+    {
+
+        [Fact]
+        public void AccessModifier_Public()
+        {
+            var instance = new AccessModifierPublic { Property = 1 };
+
+            Assert.Equal(5, AccessModifierPublic.PropertyLens.Get(AccessModifierPublic.PropertyLens.Set(5, instance)));
+        }
+
+        [Fact]
+        public void AccessModifier_Internal()
+        {
+            var instance = new AccessModifierInternal { Property = 1 };
+
+            Assert.Equal(5, AccessModifierInternal.PropertyLens.Get(AccessModifierInternal.PropertyLens.Set(5, instance)));
+        }
+
+        [Fact]
+        public void AccessModifier_Protected()
+        {
+            AccessModifierProtectedTest.Test();
+        }
+
+        [Fact]
+        public void AccessModifier_PrivateProtected()
+        {
+            AccessModifierPrivateProtectedTest.Test();
+        }
+
+        [Fact]
+        public void AccessModifier_Private()
+        {
+            AccessModifierPrivate.Test();
+        }
+    }
+}
---
+++ ThorSoft.Optics/ThorSoft.Optics.Generator.IntegrationTests/LensGenerationTests.RecordDeclaration.cs
@@ -0,0 +1,176 @@
+﻿namespace ThorSoft.Optics.Generator.IntegrationTests
+{
+    [GenerateLenses]
+    partial record class RecordClass { public int Property { get; init; } }
+
+    [GenerateLenses]
+    sealed partial record class SealedRecordClass { public int Property { get; init; } }
+
+    [GenerateLenses]
+    abstract partial record class AbstractRecordClass { public int Property { get; init; } }
+
+    record class AbstractRecordClassDerived : AbstractRecordClass;
+
+    [GenerateLenses]
+    public partial record class PublicRecordClass { public int Property { get; init; } }
+
+    [GenerateLenses]
+    public sealed partial record class PublicSealedRecordClass { public int Property { get; init; } }
+
+    [GenerateLenses]
+    public abstract partial record class PublicAbstractRecordClass { public int Property { get; init; } }
+
+    public record class PublicAbstractRecordClassDerived : PublicAbstractRecordClass;
+
+    [GenerateLenses]
+    internal partial record class InternalRecordClass { public int Property { get; init; } }
+
+    [GenerateLenses]
+    internal sealed partial record class InternalSealedRecordClass { public int Property { get; init; } }
+
+    [GenerateLenses]
+    internal abstract partial record class InternalAbstractRecordClass { public int Property { get; init; } }
+
+    internal record class InternalAbstractRecordClassDerived : InternalAbstractRecordClass;
+
+    [GenerateLenses]
+    partial record struct RecordStruct { public int Property { get; init; } }
+
+    [GenerateLenses]
+    readonly partial record struct ReadonlyRecordStruct { public int Property { get; init; } }
+
+    [GenerateLenses]
+    public partial record struct PublicRecordStruct { public int Property { get; init; } }
+
+    [GenerateLenses]
+    public readonly partial record struct PublicReadonlyRecordStruct { public int Property { get; init; } }
+
+    [GenerateLenses]
+    internal partial record struct InternalRecordStruct { public int Property { get; init; } }
+
+    [GenerateLenses]
+    internal readonly partial record struct InternalReadonlyRecordStruct { public int Property { get; init; } }
+
+    public sealed partial class LensGenerationTests
+    {
+        [Fact]
+        public void RecordDeclaration_RecordClass()
+        {
+            var instance = new RecordClass { Property = 1 };
+
+            Assert.Equal(instance.Property, RecordClass.PropertyLens.Get(instance));
+        }
+
+        [Fact]
+        public void RecordDeclaration_SealedRecordClass()
+        {
+            var instance = new SealedRecordClass { Property = 1 };
+
+            Assert.Equal(instance.Property, SealedRecordClass.PropertyLens.Get(instance));
+        }
+
+        [Fact]
+        public void RecordDeclaration_AbstractRecordClass()
+        {
+            var instance = new AbstractRecordClassDerived { Property = 1 };
+
+            Assert.Equal(instance.Property, AbstractRecordClassDerived.PropertyLens.Get(instance));
+        }
+
+        [Fact]
+        public void RecordDeclaration_PublicRecordClass()
+        {
+            var instance = new PublicRecordClass { Property = 1 };
+
+            Assert.Equal(instance.Property, PublicRecordClass.PropertyLens.Get(instance));
+        }
+
+        [Fact]
+        public void RecordDeclaration_PublicSealedRecordClass()
+        {
+            var instance = new PublicSealedRecordClass { Property = 1 };
+
+            Assert.Equal(instance.Property, PublicSealedRecordClass.PropertyLens.Get(instance));
+        }
+
+        [Fact]
+        public void RecordDeclaration_PublicAbstractRecordClass()
+        {
+            var instance = new PublicAbstractRecordClassDerived { Property = 1 };
+
+            Assert.Equal(instance.Property, PublicAbstractRecordClassDerived.PropertyLens.Get(instance));
+        }
+
+        [Fact]
+        public void RecordDeclaration_InternalRecordClass()
+        {
+            var instance = new InternalRecordClass { Property = 1 };
+
+            Assert.Equal(instance.Property, InternalRecordClass.PropertyLens.Get(instance));
+        }
+
+        [Fact]
+        public void RecordDeclaration_InternalSealedRecordClass()
+        {
+            var instance = new InternalSealedRecordClass { Property = 1 };
+
+            Assert.Equal(instance.Property, InternalSealedRecordClass.PropertyLens.Get(instance));
+        }
+
+        [Fact]
+        public void RecordDeclaration_InternalAbstractRecordClass()
+        {
+            var instance = new InternalAbstractRecordClassDerived { Property = 1 };
+
+            Assert.Equal(instance.Property, InternalAbstractRecordClassDerived.PropertyLens.Get(instance));
+        }
+
+        [Fact]
+        public void RecordDeclaration_RecordStruct()
+        {
+            var instance = new RecordStruct { Property = 1 };
+
+            Assert.Equal(instance.Property, RecordStruct.PropertyLens.Get(instance));
+        }
+
+        [Fact]
+        public void RecordDeclaration_ReadonlyRecordStruct()
+        {
+            var instance = new ReadonlyRecordStruct { Property = 1 };
+
+            Assert.Equal(instance.Property, ReadonlyRecordStruct.PropertyLens.Get(instance));
+        }
+
+        [Fact]
+        public void RecordDeclaration_PublicRecordStruct()
+        {
+            var instance = new PublicRecordStruct { Property = 1 };
+
+            Assert.Equal(instance.Property, PublicRecordStruct.PropertyLens.Get(instance));
+        }
+
+        [Fact]
+        public void RecordDeclaration_PublicReadonlyRecordStruct()
+        {
+            var instance = new PublicReadonlyRecordStruct { Property = 1 };
+
+            Assert.Equal(instance.Property, PublicReadonlyRecordStruct.PropertyLens.Get(instance));
+        }
+
+        [Fact]
+        public void RecordDeclaration_InternalRecordStruct()
+        {
+            var instance = new InternalRecordStruct { Property = 1 };
+
+            Assert.Equal(instance.Property, InternalRecordStruct.PropertyLens.Get(instance));
+        }
+
+        [Fact]
+        public void RecordDeclaration_InternalReadonlyRecordStruct()
+        {
+            var instance = new InternalReadonlyRecordStruct { Property = 1 };
+
+            Assert.Equal(instance.Property, InternalReadonlyRecordStruct.PropertyLens.Get(instance));
+        }
+    }
+}
