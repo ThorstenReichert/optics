bfd6a5e774d934f07b69efd90b474cb41c4eaa2d
Thorsten Reichert <th.reichert@gmx.net>
2025-12-07 22:13:15 +0100

    exclude marker method from code coverage

--- ThorSoft.Optics/ThorSoft.Optics/Lens.cs
+++ ThorSoft.Optics/ThorSoft.Optics/Lens.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq.Expressions;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Linq.Expressions;
 
 namespace ThorSoft.Optics
 {
@@ -46,6 +47,7 @@ namespace ThorSoft.Optics
         /// <returns>
         ///     A <see cref="Lens{T, U}"/> focusing on the nested property selected by <paramref name="propertySelector"/>.
         /// </returns>
+        [ExcludeFromCodeCoverage]
         public static Lens<T, U> Focus<U>(Expression<Func<T, U>> propertySelector)
         {
             throw new NotImplementedException("Method is only a marker to be intercepted via the accompanying source-generator");
